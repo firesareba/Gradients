5d6db92bef14ca4b717b55d4b396e85a73e1fb4f
firesareba <101755187+firesareba@users.noreply.github.com>
2025-12-13 22:55:49 -0800

    Make canvas changable
    I made 2 functions, getRGB which is same as before but moved into func, and setPixel which allows you to set the color for any pixel in the canvas by drawing a 1px by 1px rectangle with provided color. To set the whole canvas, i have temporarily made 2 nested forloops to loop over every single pixel and set it

--- script.js
+++ script.js
@@ -17,13 +17,31 @@ const hex_to_dec = {
     'f':15,
 };
 
+let c = document.getElementById("gradient-surface");
+let ctx = c.getContext("2d");
+
 const color_picker = document.getElementById("color-picker");
+
 color_picker.addEventListener('change', (event) => {
+    for (let i = 0; i < 300; i++){
+        for (let j = 0; j < 300; j++){
+            setPixel(i, j, getRGB())
+        }
+    }
+})
+
+function getRGB() {
     let hex = color_picker.value;
     var rgb = [
         hex_to_dec[hex[1]]*16+hex_to_dec[hex[2]],
         hex_to_dec[hex[3]]*16+hex_to_dec[hex[4]],
         hex_to_dec[hex[5]]*16+hex_to_dec[hex[6]]
     ];
-    console.log(rgb);
-})
+    return rgb;
+}
+
+function setPixel(x, y, rgb) {
+    ctx.fillStyle = `rgb(${rgb[0]}, ${rgb[1]}, ${rgb[2]})`;
+    ctx.fillRect(x, y, 1, 1)
+}
+
