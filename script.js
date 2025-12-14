222b777ac9728ccf1e481d48593cecc17abab8c1
Simha <simha@Simhas-Virtual-Machine.local>
2025-12-13 21:51:33 -0800

    Slight improvements to base code. No logical implementations yet
    I'm really strugling cause i usually use python but i have to do js for this :(. Basically, I added a canvas which i think is how you implement a pygame surface in html, and a input to get the color which is detected in the js file

--- script.js
+++ script.js
@@ -1,1 +1,23 @@
-//nothing yet, but htis is where the loginc will go
+const hex_to_dec = {
+    '0':0,
+    '1':1,
+    '2':2,
+    '3':3,
+    '4':4,
+    '5':5,
+    '6':6,
+    '7':7,
+    '8':8,
+    '9':9,
+    'A':10,
+    'B':11,
+    'C':12,
+    'D':13,
+    'E':14,
+    'F':15,
+};
+
+const color_picker = document.getElementById("color-picker");
+color_picker.addEventListener('change', (event) => {
+    console.log(color_picker.value);
+})
