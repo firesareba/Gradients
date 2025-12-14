5754daba7a6f11f5ed387aba6e74327b5d64f504
firesareba <101755187+firesareba@users.noreply.github.com>
2025-12-13 22:25:57 -0800

    Convert hex to rgb
    Trust me this took way too long for me to not commit

--- script.js
+++ script.js
@@ -9,15 +9,21 @@ const hex_to_dec = {
     '7':7,
     '8':8,
     '9':9,
-    'A':10,
-    'B':11,
-    'C':12,
-    'D':13,
-    'E':14,
-    'F':15,
+    'a':10,
+    'b':11,
+    'c':12,
+    'd':13,
+    'e':14,
+    'f':15,
 };
 
 const color_picker = document.getElementById("color-picker");
 color_picker.addEventListener('change', (event) => {
-    console.log(color_picker.value);
+    let hex = color_picker.value;
+    var rgb = [
+        hex_to_dec[hex[1]]*16+hex_to_dec[hex[2]],
+        hex_to_dec[hex[3]]*16+hex_to_dec[hex[4]],
+        hex_to_dec[hex[5]]*16+hex_to_dec[hex[6]]
+    ];
+    console.log(rgb);
 })
