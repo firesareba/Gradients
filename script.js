08ee7821045b4ee0996237db4bfaebaab4c0d935
firesareba <101755187+firesareba@users.noreply.github.com>
2025-12-15 20:50:02 -0800

    FOUND THE FORMULA
    formula is rotating around average distance! you do this by taking difference of avg and distance and then * 2 and then add to original distance. Reason this works is the first time you add the difference will get you to the avg(because avg-dist+dist = avg) and the second time will get you ahead of avg if it was originally behind, or vise versa. This also maintains sum of multipliers=1 because the if you take the sum of all diferences from avg(With the sign, so + or -) it is 0. So if the total increase in distances is x, the total decrease is also x

--- script.js
+++ script.js
@@ -11,13 +11,19 @@ function gradient(top_left, bottom_right) {
         for (let y = 0; y < c.height; y++){
             var top_distance = Math.abs(0-x) + Math.abs(0-y);
             var bottom_distance = Math.abs(c.width-x) + Math.abs(c.height-y);
+
             var sum = top_distance+bottom_distance;
             var avg = sum/2;
-            var top_rotated_distance = Math.abs(avg-top_distance);
-            var bottom_rotated_distance = Math.abs(avg-bottom_distance);
+
+            top_distance = ((avg-top_distance)*2)+top_distance;
+            bottom_distance = ((avg-bottom_distance)*2)+bottom_distance;
+
+            var top_multiplier = (top_distance/sum);
+            var bottom_multiplier = (bottom_distance/sum);
+
             var pixel_color = [0, 0, 0];
             for (let idx = 0; idx < 3; idx++){
-                var color = top_left[idx]*(top_rotated_distance/sum) + bottom_right[idx]*(bottom_rotated_distance/sum);
+                var color = (top_left[idx]*top_multiplier) + (bottom_right[idx]*bottom_multiplier);
                 pixel_color[idx] = color;
             }
             setPixel(x, y, pixel_color);
